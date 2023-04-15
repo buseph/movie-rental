4274f9e266d5aaaf2a58a1ec6d436268c189eb4b
buseph <76971029+buseph@users.noreply.github.com>
2023-04-15 12:09:42 +0800

    update april 15

--- movie-rental/dbo/Tables/rental.sql
+++ movie-rental/dbo/Tables/rental.sql
@@ -3,7 +3,8 @@
     [customerId] UNIQUEIDENTIFIER NOT NULL,
     [movieId]    UNIQUEIDENTIFIER NOT NULL,
     [rentalDate] DATETIME         NOT NULL,
-    [returnDate] DATETIME         NOT NULL,
+    [returnDate] DATETIME         NULL,
+    [isReturned] BIT NOT NULL, 
     CONSTRAINT [table1_pkey2] PRIMARY KEY CLUSTERED ([id] ASC),
     CONSTRAINT [rental_customer_id_fkey] FOREIGN KEY ([customerId]) REFERENCES [dbo].[customer] ([id]),
     CONSTRAINT [rental_movie_id_fkey] FOREIGN KEY ([movieId]) REFERENCES [dbo].[movie] ([id])
